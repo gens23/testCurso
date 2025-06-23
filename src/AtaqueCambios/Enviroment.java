8d8c2181335826589278885710291fdda4563297
USUARIO <USUARIO@192.168.1.117>
2025-06-23 11:46:53 -0600

    ahora creo que si lo subi bien

---
+++ src/AtaqueCambios/Enviroment.java
@@ -0,0 +1,26 @@
+package AtaqueCambios;
+
+public class Enviroment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Entidad entidad = new Entidad(0, 0, 0, 0, 0);
+		
+		Entidad player = new Player(50,20,0,1,10);
+		
+		Entidad enemy = new Enemy(0, 0, 0, 0, 0);
+		
+		Entidad npc = new Npc(0, 0, 0, 0, 0);
+		
+		Entidad atacante = new Player(50,20,0,1,10);
+		Entidad victima = new Enemy(30,20,0,1,10);
+		atacante.atacar(objetivo);
+		
+		System.out.println("DROPEO: \nentidad " + entidad.dropeo() + "\n Enemy " + enemy.dropeo() + "\n NPC " + npc.dropeo());
+		
+		
+		
+	}
+
+}
